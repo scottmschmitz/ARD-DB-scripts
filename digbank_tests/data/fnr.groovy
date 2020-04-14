4159e300d0fe08ce83dd17431e3c6a165c77d291
scott_schmitz <scott_schmitz@scotts-tdm-server.us-east1-b.c.demos-esd-ct-presales.internal>
2020-04-14 20:02:29 +0000

    update for login

--- digbank_tests/data/fnr.groovy
+++ digbank_tests/data/fnr.groovy
@@ -1,5 +1,13 @@
 import jenkins.model.*
 jenkins = Jenkins.instance
 
-def token = login()
-echo token
+def login(){
+              def response = httpRequest customHeaders: [[maskValue: false, name: 'Authorization', value: 'Basic QWRtaW5pc3RyYXRvcjptYXJtaXRl']], httpMode: 'POST', outputFile: 'test.txt',
+              url: 'https://scotts-tdm-serv:8443/TestDataManager/user/login'
+              def body = readJSON file: '', text: response.content
+              echo body['token']
+              def timestamp = EXECUTION_TIME.getTime()
+              echo timestamp.toString()
+              token = body['token']
+              return token
+    } //end login

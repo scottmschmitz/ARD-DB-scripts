f82bc3dd2cc4ae90e1ce2372dc7e76c42b9865ad
scott_schmitz <scott_schmitz@scotts-tdm-server.us-east1-b.c.demos-esd-ct-presales.internal>
2020-04-27 17:10:09 +0000

    fix findData

--- digbank_tests/data/fnr.groovy
+++ digbank_tests/data/fnr.groovy
@@ -49,8 +49,7 @@ def login(){
     } //end login
 def findData(String projectId, String versionId, String modelId, String environmentId,String token){
               def response = httpRequest customHeaders: [[maskValue: false, name: 'Authorization', value: 'Bearer ' +token]],contentType: 'APPLICATION_JSON', httpMode: 'POST', responseHandle: 'LEAVE_OPEN',
-                requestBody: "{"environmentId":"206","filters":[{"attributeName":"id","entityName":"user_profile","schema":"dbo","dataSource":"SDS","operator":"GREATER_THAN_OR_EQUAL_TO","values":["1000"]}]
-                              }",
+                requestBody: "{"environmentId":"206","filters":[{"attributeName":"id","entityName":"user_profile","schema":"dbo","dataSource":"SDS","operator":"GREATER_THAN_OR_EQUAL_TO","values":["1000"]}]}",
               url: 'https://scotts-tdm-serv:8443/TDMDataReservationService/api/ca/v1/testDataModels/212/actions/find?projectId=2387&versionId=2388'
               def body = readJSON file: '', text: response.content
               echo response.content
